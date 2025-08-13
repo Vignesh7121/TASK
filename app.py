0afaca24b9a68e6afc9c474a20b0141c5cf7fc3b
Vignesh7121 <vickysha7121@gmail.com>
2025-08-13 13:38:33 +0530

    last

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config.update(
     MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS', 'True').lower() in ['true', '1', 'yes'],
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD'),
-    MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER', 'visurainfotechcontrol@gmail.com')
+    MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER', 'vickysha7121@gmail.com')
 )
 
 mail = Mail(app)
